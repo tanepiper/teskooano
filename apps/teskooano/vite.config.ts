f7f064f11fb78bc70452743e0d82d35584f369d4
Tane Piper <me@tane.dev>
2025-04-18 13:20:25 +0200

    build: fix base path for gh pages

--- apps/teskooano/vite.config.ts
+++ apps/teskooano/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'; // Import the plugin
 
+
+const basePath = process.env.CI ? '/teskooano/' : '/';
+
+
 export default defineConfig({
   plugins: [
     glsl() // Add the plugin to the plugins array
   ],
+  base: basePath,
   server: {
     port: 3000,
   },
