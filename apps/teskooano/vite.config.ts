241fc5622fabbc70ec2ab4695c587f27dd7840c2
Tane Piper <me@tane.dev>
2025-04-21 10:14:25 +0200

    feat: add pwa wrapper

--- apps/teskooano/vite.config.ts
+++ apps/teskooano/vite.config.ts
@@ -1,12 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import glsl from "vite-plugin-glsl"; // Import the plugin
+import { VitePWA } from "vite-plugin-pwa";
 
 const basePath = process.env.CI ? "/teskooano" : "/";
 
 export default defineConfig({
   plugins: [
     glsl(), // Add the plugin to the plugins array
-  ],
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Teskooano 3D N-Body Simulator",
+        short_name: "Teskooano",
+        description: "N-Body Physics Simulation",
+        theme_color: "#2a2a3e",
+        icons: [
+          {
+            src: "pwa-192x192.png", // Make sure you have this icon in public/
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png", // And this one too
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ] as PluginOption[],
   base: basePath,
 
   server: {
