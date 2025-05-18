3373e45317e0cda2316e5e21e841043f1a55177d
Tane Piper <me@tane.dev>
2025-05-19 00:31:38 +0200

    remove: old orbit manager

--- packages/renderer/threejs-orbits/src/index.ts
+++ packages/renderer/threejs-orbits/src/index.ts
@@ -4,21 +4,7 @@
  * Visualization of orbital paths using ThreeJS
  */
 
-// Original exports for backward compatibility
-import {
-  OrbitManager,
-  VisualizationMode as LegacyVisualizationMode,
-} from "./OrbitManager";
-export {
-  OrbitManager,
-  VisualizationMode as LegacyVisualizationMode,
-} from "./OrbitManager";
-export * from "./orbit-manager";
-
-// New refactored exports
-// These are the preferred imports for new code
-export { OrbitsManager, VisualizationMode } from "./core";
-export * from "./core/SharedMaterials";
+export * from "./core";
 export * from "./keplerian";
 export * from "./verlet";
 export * from "./utils";
