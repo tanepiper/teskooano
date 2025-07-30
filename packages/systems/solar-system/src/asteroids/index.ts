034b2f6229660d0a8011a5c7146e47a1c6792b0e
Tane Piper <me@tane.dev>
2025-07-30 19:01:41 +0200

    system: add missing asteroid

--- packages/systems/solar-system/src/asteroids/index.ts
+++ packages/systems/solar-system/src/asteroids/index.ts
@@ -1,5 +1,6 @@
 import { eros } from "./433-eros";
 import { apophis } from "./99942-apophis";
 import { vesta } from "./vesta";
+import { pallas } from "./pallas";
 
-export const asteroids = [eros, apophis, vesta];
+export const asteroids = [eros, apophis, vesta, pallas];
